966ebccec83d15e0ac42eea7ebaaf48713ad9d81
Momingcoder <kemingy94@gmail.com>
2018-02-27 19:19:30 +0800

    add custom regex test

---
+++ test/test_custom.py
@@ -0,0 +1,14 @@
+from plane import extract, replace, build_new_regex
+
+def test_custom_regex():
+    custom_regex = build_new_regex('custom', r'(?i)(one|two|three)', '<NUM>')
+    text = 'One of the two will be used. Which one is undefined.'
+    num = ['One', 'two', 'one']
+    expect = '<NUM> of the <NUM> will be used. Which <NUM> is undefined.'
+
+    result = list(extract(text, custom_regex))
+    assert len(result) == len(num)
+    for i in range(len(num)):
+        assert num[i] == result[i].value
+
+    assert replace(text, custom_regex) == expect
