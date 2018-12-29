1f8a981be1ca8dd13b8bd028bd29b91ae3defa19
keming <kemingy94@gmail.com>
2018-12-29 17:20:15 +0800

    update version: 0.1.4

--- plane/__init__.py
+++ plane/__init__.py
@@ -5,4 +5,4 @@ from plane.pipeline import Pipeline  # noqa: F401
 from plane.pattern import HTML, EMAIL, SPACE, TELEPHONE, URL  # noqa: F401
 from plane.pattern import ASCII_WORD, CHINESE, CJK, CHINESE_WORDS  # noqa: F401
 
-__version__ = '0.1.3'
+__version__ = '0.1.4'
