4b194620c1df5880bcfe24ca63fe1aa8729cdac6
keming <kemingy94@gmail.com>
2019-02-13 10:55:30 +0800

    change version to 0.1.5

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='plane',
-    version='0.1.4',
+    version='0.1.5',
     description='A lib for text preprocessing',
     long_description=long_description,
     author='Keming Yang',
