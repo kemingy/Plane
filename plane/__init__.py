033ff2fd3edad3aa75c327ee51be0bf1d167fecc
keming <kemingy94@gmail.com>
2019-03-05 16:58:10 +0800

    add viet thai

--- plane/__init__.py
+++ plane/__init__.py
@@ -4,5 +4,6 @@ from plane.plane import Plane  # noqa: F401
 from plane.pipeline import Pipeline  # noqa: F401
 from plane.pattern import HTML, EMAIL, SPACE, TELEPHONE, URL  # noqa: F401
 from plane.pattern import ASCII_WORD, CHINESE, CJK, CHINESE_WORDS  # noqa: F401
+from plane.pattern import ENGLISH, VIETNAMESE, THAI, NUMBER  # noqa: F401
 
 __version__ = '0.1.6'
