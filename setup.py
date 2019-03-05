3643a6670a65ebdab2ab6197bf5060174fff4bfc
keming <kemingy94@gmail.com>
2019-03-05 16:59:05 +0800

    update version: 0.1.7

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='plane',
-    version='0.1.6',
+    version='0.1.7',
     description='A lib for text preprocessing',
     long_description=long_description,
     long_description_content_type='text/markdown',
